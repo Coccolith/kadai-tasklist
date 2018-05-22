f76956b32eecc05db33fba9d48d501b823e4cb3a
EC2 Default User <ec2-user@ip-172-31-38-117.us-east-2.compute.internal>
2018-05-22 05:34:37 +0000

    bootstrap design 2

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 @section('content')
 <h1>id = {{ $task->id }}のタスク詳細ページ</h1>
-<p>{{ $task->content }}</p>
+<p>タスク: {{ $task->content }}</p>
+<p>ステータス: {{ $task->status }}</p>
 {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
 
 {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
