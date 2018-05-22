59cb4b659cc8d6e2ceb35b40a2aeda13cf176a45
EC2 Default User <ec2-user@ip-172-31-38-117.us-east-2.compute.internal>
2018-05-22 04:21:29 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,11 @@
 @section('content')
 <h1>id: {{ $task->id }}のタスク編集ページ</h1>
 {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+{!! Form::label('content', 'タスク:') !!}
 {!! Form::text('content') !!}
+
+{!! Form::label('status', 'ステータス:') !!}
+{!! Form::text('status') !!}
 
 {!! Form::submit('更新') !!}
 {!! Form::close() !!}
